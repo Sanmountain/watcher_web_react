e3aa6a3f277042098a5f0cecffec7edf26bcd5d6
Sanmountain <tksgk7777@naver.com>
2023-05-12 15:51:08 +0900

    Revert "230511 기초자료_코드 부분 CRUD 완료"
    This reverts commit 4c9db8457655a0978aabd7856bd76922f96924f8.

--- src/components/work/Loading.js
+++ src/components/work/Loading.js
@@ -3,28 +3,24 @@ import Spinner from "../../styles/Spin-1s-200px.gif";
 
 export const Loading = () => {
   return (
-    <div>
+    <div
+      style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        backgroundColor: "white",
+      }}
+    >
       <div
         style={{
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "rgba(80,80,80,0.3)",
-          textAlign: "center",
+          display: "block",
+          fontSize: "40px",
         }}
       >
-        <div
-          style={{
-            display: "block",
-            fontSize: "40px",
-            color: "white",
-          }}
-        >
-          Loading
-        </div>
-        <img src={Spinner} alt="로딩중" width="70%" />
+        Loading
       </div>
+      <img src={Spinner} alt="로딩중" width="70%" />
     </div>
   );
 };
