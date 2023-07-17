00749d7e34c01bcc472ef442d7e2bc0dc40d14d1
Sanmountain <tksgk7777@naver.com>
2023-07-17 15:27:21 +0900

    + css 수정

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Login from "./routes/Login";
 import Work from "./routes/Work";
 import Vass from "./routes/VASS";
 import Header from "./components/Header";
 import VassCam from "./routes/VassCam";
 
-function App() {
+function Application() {
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <Header />
+    <>
+      {location.pathname !== "/" && <Header />}
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/work" element={<Work />}></Route>
         <Route path="/vass" element={<Vass />}></Route>
         <Route path="/vasscam" element={<VassCam />}></Route>
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Application />
     </BrowserRouter>
   );
 }
