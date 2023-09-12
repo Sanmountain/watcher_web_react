b35e7255f828cda7f624863bc22de7696fffb9ab
HyerinJeon <hyerin0930@gmail.com>
2023-09-12 13:16:15 +0900

    Table Component 추가

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,5 +1,26 @@
 import Filter from "../components/common/Filter";
+import Table from "../components/common/Table";
 
 export default function Work() {
-  return <Filter></Filter>;
+  const title = [
+    "No.",
+    "업무",
+    "영업소",
+    "상대영업소",
+    "차량",
+    "송장",
+    "상대",
+    "날짜",
+    "시간",
+    "사원",
+  ];
+
+  const contents = [""];
+
+  return (
+    <>
+      <Filter />
+      <Table title={title} contents={contents} columns={10} />
+    </>
+  );
 }
---
+++ src/types/Table.types.ts
@@ -0,0 +1,5 @@
+export interface ITableProps {
+  title: string[];
+  contents: string[];
+  columns: number;
+}
