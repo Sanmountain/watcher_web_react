21d77edf9b61455f9e6a8363bdd489d7a2081091
HyerinJeon <hyerin0930@gmail.com>
2023-09-13 09:31:52 +0900

    Vass 상세페이지 sidebar 분기처리

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,7 +2,8 @@ import * as S from "../../styles/layout/Sidebar.styles";
 import VassIcon from "../../assets/images/sidebar/icon_vass.png";
 import shoppingMallIcon from "../../assets/images/sidebar/icon_shoppingMall.png";
 import S4Image from "../../assets/images/sidebar/img_S4.png";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import CommonButton from "../common/CommonButton";
 
 export default function Sidebar() {
   const menuList = [
@@ -10,7 +11,9 @@ export default function Sidebar() {
     { label: "송장조회", path: "/work" },
     { label: "VASS", path: "/vass" },
   ];
+
   const navigate = useNavigate();
+  const params = useParams();
 
   const onClickMenu = (path: string) => {
     navigate(path);
@@ -30,22 +33,49 @@ export default function Sidebar() {
         ))}
       </S.MenuContainer>
 
-      <S.ShoppingMallContainer>
-        <S.ShoppingMallLogoContainer>
-          <a
-            href="http://jhc0213.godomall.com/main/index.php"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <S.ShoppingMallImage src={shoppingMallIcon} />
-            <S.ShoppingMallButton>쇼핑몰 바로가기 &gt;</S.ShoppingMallButton>
-          </a>
-        </S.ShoppingMallLogoContainer>
+      {params.invoiceNumber ? (
+        <S.ShoppingMallContainer>
+          <S.InvoiceButtonContainer>
+            <CommonButton
+              contents="송장번호 조회"
+              onClickFn={() => console.log("ddd")}
+            />
+          </S.InvoiceButtonContainer>
+          <S.InvoiceInput placeholder="송장번호를 입력해주세요" />
 
-        <S.ProductImageContainer>
-          <S.Image src={S4Image} />
-        </S.ProductImageContainer>
-      </S.ShoppingMallContainer>
+          <S.InvoiceInfoContainer>
+            <p>현재 송장번호</p>
+            <S.InvoiceInfo>36336157723</S.InvoiceInfo>
+          </S.InvoiceInfoContainer>
+
+          <S.InvoiceInfoContainer>
+            <p>이전 송장번호</p>
+            <S.InvoiceInfo className="prev">36336157723</S.InvoiceInfo>
+          </S.InvoiceInfoContainer>
+
+          <S.TradeSubInfoContainer>
+            <p>담당직원 : 홍길동</p>
+            <p>배송출발</p>
+          </S.TradeSubInfoContainer>
+        </S.ShoppingMallContainer>
+      ) : (
+        <S.ShoppingMallContainer>
+          <S.ShoppingMallLogoContainer>
+            <a
+              href="http://jhc0213.godomall.com/main/index.php"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <S.ShoppingMallImage src={shoppingMallIcon} />
+              <S.ShoppingMallButton>쇼핑몰 바로가기 &gt;</S.ShoppingMallButton>
+            </a>
+          </S.ShoppingMallLogoContainer>
+
+          <S.ProductImageContainer>
+            <S.Image src={S4Image} />
+          </S.ProductImageContainer>
+        </S.ShoppingMallContainer>
+      )}
     </S.Container>
   );
 }
