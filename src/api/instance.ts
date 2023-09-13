f65b68bf3b5e7e1feb58967dbb6ea546cc062894
HyerinJeon <hyerin0930@gmail.com>
2023-09-13 14:24:06 +0900

    instance 추가

---
+++ src/api/instance.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/AndyLotte`,
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      return response.data;
+    }
+  },
+
+  (error) => {
+    console.log(error);
+  },
+);
