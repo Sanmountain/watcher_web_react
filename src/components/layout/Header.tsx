abc65b14809417e697787fb5b73ccadc8351a631
HyerinJeon <hyerin0930@gmail.com>
2023-09-14 17:28:18 +0900

    logout reset 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,21 +3,30 @@ import * as S from "../../styles/layout/Header.styles";
 import { loginState } from "../../stores/loginState";
 import { useNavigate } from "react-router";
 import { vassListState } from "../../stores/vass/vassListState";
+import { nowVassDetailState } from "../../stores/vass/nowVassDetailState";
+import { prevVassDetailState } from "../../stores/vass/prevVassDetailState";
 
 export default function Header() {
   const [login, setLogin] = useRecoilState(loginState);
   const resetLogin = useResetRecoilState(loginState);
   const resetVassList = useResetRecoilState(vassListState);
+  const resetNowVassDetail = useResetRecoilState(nowVassDetailState);
+  const resetPrevVassDetail = useResetRecoilState(prevVassDetailState);
 
   const navigate = useNavigate();
 
   const onClickLogout = () => {
     if (login.isUserIdStored) {
       setLogin({ ...login, isLogin: false, branchName: "", localIP: "" });
+      resetVassList();
+      resetNowVassDetail();
+      resetPrevVassDetail();
     } else if (!login.isUserIdStored) {
       resetLogin();
+      resetVassList();
+      resetNowVassDetail();
+      resetPrevVassDetail();
     }
-    resetVassList();
 
     navigate("/login");
   };
