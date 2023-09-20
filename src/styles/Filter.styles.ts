03a5ec7c3004ba0e32ab73e8aaa96a772692d841
HyerinJeon <hyerin0930@gmail.com>
2023-09-20 11:31:45 +0900

    Filter CSS 수정

--- src/styles/Filter.styles.ts
+++ src/styles/Filter.styles.ts
@@ -1,32 +1,78 @@
 import styled from "styled-components";
 import { font } from "./typography";
 import { colors } from "./palette";
+import { mediaQuery } from "./mediaQuery";
+import { breakPoints } from "./breakPoints";
 
-export const Container = styled.div`
+export const Container = styled.div<{ $isWorkPage: boolean }>`
   display: grid;
-  grid-template-columns: 1.3fr 1fr 1.1fr;
+  grid-template-columns: 1.3fr 1.1fr 1fr;
   align-items: center;
-  width: 99.4%;
+  width: 97.8%;
+  min-width: calc(${breakPoints.medium}px - 230px);
   height: 65px;
+  margin-left: 1%;
   position: sticky;
   top: 70px;
   background-color: ${colors.black[1000]};
+
+  ${mediaQuery.large(`
+    grid-template-columns: 1.5fr 1.1fr 0.8fr;
+  `)}
+
+  ${(props) =>
+    props.$isWorkPage
+      ? mediaQuery.largeMedium(`
+          grid-template-columns: 1.3fr 1fr;
+          grid-template-rows: 1fr 1fr;
+          height: 110px;
+        `)
+      : mediaQuery.largeMedium(`
+          grid-template-columns: 1.3fr 1fr;
+  `)}
 `;
 
 export const FilterContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
 
   width: 100%;
   height: 100%;
   gap: 20px;
   font-size: ${font.fontSize[200]};
+
+  ${mediaQuery.large(`
+    gap: 10px;
+  `)}
+
+  ${mediaQuery.largeMedium(`
+      justify-content: center; 
+  `)}
+
+  &.noDisplay {
+    display: none;
+
+    ${mediaQuery.largeMedium(`
+      display: flex; 
+  `)}
+  }
+
+  &.register {
+    justify-content: center;
+
+    ${mediaQuery.largeMedium(`
+      justify-content: center;
+  `)}
+  }
 `;
 
 export const FilterTitle = styled.div`
   font-size: ${font.fontSize[100]};
   font-weight: 700;
+
+  ${mediaQuery.large(`
+    font-size: ${font.fontSize[200]};
+  `)}
 `;
 
 export const SelectBox = styled.select`
