9c68430853324478aca33b0eca0b2bf1720ea15d
HyerinJeon <hyerin0930@gmail.com>
2023-10-12 10:55:20 +0900

    vass 분기처리

--- src/api/vass/getVassDateList.tsx
+++ src/api/vass/getVassDateList.tsx
@@ -2,7 +2,7 @@ import { useRecoilValue, useSetRecoilState } from "recoil";
 import { loginState } from "../../stores/loginState";
 import { useMutation } from "react-query";
 import { IWorkListResponse } from "../../types/Work.types";
-import { instance } from "../instance";
+import { HanjinInstance, LogenInstance, LotteInstance } from "../instance";
 import dayjs from "dayjs";
 import { vassFilterState } from "../../stores/vass/vassFilterState";
 import { vassListState } from "../../stores/vass/vassListState";
@@ -13,49 +13,148 @@ export const getVassDateList = () => {
   const filterOption = useRecoilValue(vassFilterState);
   const setWorkList = useSetRecoilState(vassListState);
 
-  return useMutation<IWorkListResponse, unknown, void, unknown>(
-    "getVassDateList",
-    () =>
-      instance.post("/lose", {
-        api: "dvInAll",
-        data: [
-          {
-            start_time: `${dayjs(filterOption.date).format(
-              "YYYY-MM-DD",
-            )} 00:00:00`,
-            end_time: `${dayjs(filterOption.date).format(
-              "YYYY-MM-DD",
-            )} 23:59:59`,
-            bran_cd: login.branchCode,
-            longTime: "",
-          },
-        ],
-      }),
-    {
-      onSuccess: (data) => {
-        if (data.result === "00") {
-          let filteringData: any;
+  // NOTE 로젠
+  if (login.company === "LOGEN") {
+    return useMutation<IWorkListResponse, unknown, void, unknown>(
+      "getVassDateList",
+      () =>
+        LogenInstance.post("/lose", {
+          api: "dvInAll",
+          data: [
+            {
+              start_time: `${dayjs(filterOption.date).format(
+                "YYYY-MM-DD",
+              )} 00:00:00`,
+              end_time: `${dayjs(filterOption.date).format(
+                "YYYY-MM-DD",
+              )} 23:59:59`,
+              bran_cd: login.branchCode,
+              longTime: "",
+            },
+          ],
+        }),
+      {
+        onSuccess: (data) => {
+          if (data.result === "00") {
+            let filteringData: any;
 
-          // NOTE 도착
-          if (filterOption.receivingShipment === "receive") {
-            filteringData = data.data.filter((item) => item.tm_dv === "21");
-          }
-          // NOTE 발송
-          else if (filterOption.receivingShipment === "shipment") {
-            filteringData = data.data.filter((item) => item.tm_dv === "20");
-          }
+            // NOTE 도착
+            if (filterOption.receivingShipment === "receive") {
+              filteringData = data.data.filter((item) => item.tm_dv === "60");
+            }
+            // NOTE 발송
+            else if (filterOption.receivingShipment === "shipment") {
+              filteringData = data.data.filter((item) => item.tm_dv === "30");
+            }
 
-          setWorkList(filteringData);
-          Swal.fire({
-            icon: "success",
-            title: "조회 성공",
-            confirmButtonText: "확인",
-          });
-        }
+            setWorkList(filteringData);
+            Swal.fire({
+              icon: "success",
+              title: "조회 성공",
+              confirmButtonText: "확인",
+            });
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
       },
-      onError: (error) => {
-        console.log(error);
+    );
+  }
+  // NOTE 롯데
+  else if (login.company === "LOTTE") {
+    return useMutation<IWorkListResponse, unknown, void, unknown>(
+      "getVassDateList",
+      () =>
+        LotteInstance.post("/lose", {
+          api: "dvInAll",
+          data: [
+            {
+              start_time: `${dayjs(filterOption.date).format(
+                "YYYY-MM-DD",
+              )} 00:00:00`,
+              end_time: `${dayjs(filterOption.date).format(
+                "YYYY-MM-DD",
+              )} 23:59:59`,
+              bran_cd: login.branchCode,
+              longTime: "",
+            },
+          ],
+        }),
+      {
+        onSuccess: (data) => {
+          if (data.result === "00") {
+            let filteringData: any;
+
+            // NOTE 도착
+            if (filterOption.receivingShipment === "receive") {
+              filteringData = data.data.filter((item) => item.tm_dv === "21");
+            }
+            // NOTE 발송
+            else if (filterOption.receivingShipment === "shipment") {
+              filteringData = data.data.filter((item) => item.tm_dv === "20");
+            }
+
+            setWorkList(filteringData);
+            Swal.fire({
+              icon: "success",
+              title: "조회 성공",
+              confirmButtonText: "확인",
+            });
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
       },
-    },
-  );
+    );
+  }
+  // NOTE 한진
+  else if (login.company === "HANJIN") {
+    return useMutation<IWorkListResponse, unknown, void, unknown>(
+      "getVassDateList",
+      () =>
+        HanjinInstance.post("/lose", {
+          api: "dvInAll",
+          data: [
+            {
+              start_time: `${dayjs(filterOption.date).format(
+                "YYYY-MM-DD",
+              )} 00:00:00`,
+              end_time: `${dayjs(filterOption.date).format(
+                "YYYY-MM-DD",
+              )} 23:59:59`,
+              bran_cd: login.branchCode,
+              longTime: "",
+            },
+          ],
+        }),
+      {
+        onSuccess: (data) => {
+          if (data.result === "00") {
+            let filteringData: any;
+
+            // NOTE 도착
+            if (filterOption.receivingShipment === "receive") {
+              filteringData = data.data.filter((item) => item.tm_dv === "31");
+            }
+            // NOTE 발송
+            else if (filterOption.receivingShipment === "shipment") {
+              filteringData = data.data.filter((item) => item.tm_dv === "32");
+            }
+
+            setWorkList(filteringData);
+            Swal.fire({
+              icon: "success",
+              title: "조회 성공",
+              confirmButtonText: "확인",
+            });
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      },
+    );
+  }
 };
--- src/api/vass/getVassInvoiceList.tsx
+++ src/api/vass/getVassInvoiceList.tsx
@@ -2,7 +2,7 @@ import { useRecoilValue, useSetRecoilState } from "recoil";
 import { loginState } from "../../stores/loginState";
 import { useMutation } from "react-query";
 import { IWorkListResponse } from "../../types/Work.types";
-import { instance } from "../instance";
+import { HanjinInstance, LogenInstance, LotteInstance } from "../instance";
 import Swal from "sweetalert2";
 import { vassFilterState } from "../../stores/vass/vassFilterState";
 import { vassListState } from "../../stores/vass/vassListState";
@@ -12,42 +12,126 @@ export const getVassInvoiceList = () => {
   const filterOption = useRecoilValue(vassFilterState);
   const setWorkList = useSetRecoilState(vassListState);
 
-  return useMutation<IWorkListResponse, unknown, void, unknown>(
-    "getVassInvoiceList",
-    () =>
-      instance.post("/lose", {
-        api: "dvInAll",
-        data: [
-          {
-            barcode: filterOption.invoiceNumber,
-            bran_cd: login.branchCode,
-            longTime: "",
-            tm_dv: "20",
-            limit_count: 2,
-          },
-        ],
-      }),
-    {
-      onSuccess: (data) => {
-        if (data.result === "10") {
-          Swal.fire({
-            icon: "warning",
-            title: "송장번호를 입력해주세요.",
-            confirmButtonText: "확인",
-          });
-        } else if (data.result === "00") {
-          setWorkList(data.data);
+  // NOTE 로젠
+  if (login.company === "LOGEN") {
+    return useMutation<IWorkListResponse, unknown, void, unknown>(
+      "getVassInvoiceList",
+      () =>
+        LogenInstance.post("/lose", {
+          api: "dvInAll",
+          data: [
+            {
+              barcode: filterOption.invoiceNumber,
+              bran_cd: login.branchCode,
+              longTime: "",
+              tm_dv: "60",
+              limit_count: 2,
+            },
+          ],
+        }),
+      {
+        onSuccess: (data) => {
+          if (data.result === "10") {
+            Swal.fire({
+              icon: "warning",
+              title: "송장번호를 입력해주세요.",
+              confirmButtonText: "확인",
+            });
+          } else if (data.result === "00") {
+            setWorkList(data.data);
 
-          Swal.fire({
-            icon: "success",
-            title: "조회 성공",
-            confirmButtonText: "확인",
-          });
-        }
+            Swal.fire({
+              icon: "success",
+              title: "조회 성공",
+              confirmButtonText: "확인",
+            });
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
       },
-      onError: (error) => {
-        console.log(error);
+    );
+  }
+  // NOTE 롯데
+  else if (login.company === "LOTTE") {
+    return useMutation<IWorkListResponse, unknown, void, unknown>(
+      "getVassInvoiceList",
+      () =>
+        LotteInstance.post("/lose", {
+          api: "dvInAll",
+          data: [
+            {
+              barcode: filterOption.invoiceNumber,
+              bran_cd: login.branchCode,
+              longTime: "",
+              tm_dv: "20",
+              limit_count: 2,
+            },
+          ],
+        }),
+      {
+        onSuccess: (data) => {
+          if (data.result === "10") {
+            Swal.fire({
+              icon: "warning",
+              title: "송장번호를 입력해주세요.",
+              confirmButtonText: "확인",
+            });
+          } else if (data.result === "00") {
+            setWorkList(data.data);
+
+            Swal.fire({
+              icon: "success",
+              title: "조회 성공",
+              confirmButtonText: "확인",
+            });
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
       },
-    },
-  );
+    );
+  }
+  // NOTE 한진
+  else if (login.company === "HANJIN") {
+    return useMutation<IWorkListResponse, unknown, void, unknown>(
+      "getVassInvoiceList",
+      () =>
+        HanjinInstance.post("/lose", {
+          api: "dvInAll",
+          data: [
+            {
+              barcode: filterOption.invoiceNumber,
+              bran_cd: login.branchCode,
+              longTime: "",
+              limit_count: 2,
+            },
+          ],
+        }),
+      {
+        onSuccess: (data) => {
+          if (data.result === "10") {
+            Swal.fire({
+              icon: "warning",
+              title: "송장번호를 입력해주세요.",
+              confirmButtonText: "확인",
+            });
+          } else if (data.result === "00") {
+            setWorkList(data.data);
+
+            Swal.fire({
+              icon: "success",
+              title: "조회 성공",
+              confirmButtonText: "확인",
+            });
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      },
+    );
+  }
 };
