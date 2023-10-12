f7f7641045849476b55809608bb1b680bc37ca47
HyerinJeon <hyerin0930@gmail.com>
2023-10-12 11:55:03 +0900

    company 분기 추가 및 토글 추가

--- src/styles/Filter.styles.ts
+++ src/styles/Filter.styles.ts
@@ -69,6 +69,49 @@ export const FilterContainer = styled.div`
   }
 `;
 
+export const AutoTitle = styled.span`
+  width: 13%;
+  margin-left: 2%;
+
+  height: 28px;
+  text-align: center;
+  padding-top: 2px;
+  background-color: #dadada;
+  border: 1px solid #dadada;
+  border-radius: 5px;
+`;
+
+export const ToggleContainer = styled.div`
+  position: relative;
+
+  > .toggle-container {
+    width: 48px;
+    height: 24px;
+    border-radius: 16px;
+    background-color: ${colors.black[600]};
+  }
+  > .toggle--checked {
+    background-color: ${colors.blue[200]};
+    transition: 0.3s;
+  }
+
+  > .toggle-circle {
+    position: absolute;
+    top: 2px;
+    left: 1px;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${colors.black[1000]};
+    box-shadow: 0px 2px 4px rgba(29, 31, 33, 0.2);
+    transition: 0.3s;
+  }
+  > .toggle--checked {
+    left: 27px;
+    transition: 0.3s;
+  }
+`;
+
 export const FilterTitle = styled.div`
   font-size: ${font.fontSize[100]};
   font-weight: 700;
