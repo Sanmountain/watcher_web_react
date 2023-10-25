58e50868ed47147a62457e68cbdcb31d387d2a15
HyerinJeon <hyerin0930@gmail.com>
2023-10-25 13:17:57 +0900

    getImage scandate params 추가

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -77,6 +77,7 @@ export default function Work() {
         }
         dateLoading={isDateMutateLoading}
         invoiceLoading={isInvoiceNumberMutateLoading}
+        filterOption={filterOption}
       />
     </>
   );
