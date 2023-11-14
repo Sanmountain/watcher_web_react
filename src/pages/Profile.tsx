4b513241712cde579f9eefa6562e74310f597d77
HyerinJeon <hyerin0930@gmail.com>
2023-11-14 13:46:54 +0900

    비밀번호 변경 페이지 CSS 수정

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,24 +1,54 @@
+import { useRecoilValue } from "recoil";
 import CommonButton from "../components/common/CommonButton";
 import * as S from "../styles/Profile.styles";
+import { loginState } from "../stores/loginState";
+import { ChangeEvent, useState } from "react";
 
 export default function Profile() {
+  const login = useRecoilValue(loginState);
+  const [password, setPassword] = useState("");
+  const [confirmed, setConfirmed] = useState("");
+
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmedPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmed(e.target.value);
+  };
+
   return (
     <S.Container>
-      <S.Title>비밀번호 변경</S.Title>
-      <S.LabelInputContainer className="first">
-        <S.Label>비밀번호</S.Label>
-        <S.Input type="password" />
-      </S.LabelInputContainer>
-      <S.LabelInputContainer>
-        <S.Label>비밀번호 확인</S.Label>
-        <S.Input type="password" />
-      </S.LabelInputContainer>
-      <CommonButton
-        contents="변경하기"
-        onClickFn={() => console.log("ddd")}
-        width="20%"
-        height="45px"
-      />
+      <S.ChangeContainer>
+        <S.Title>비밀번호 변경</S.Title>
+        <S.LabelInputContainer>
+          <S.Label>지점명</S.Label>
+          <S.Input
+            value={`${login.branchName} (${login.branchCode})`}
+            readOnly
+            className="branch"
+          />
+        </S.LabelInputContainer>
+        <S.LabelInputContainer>
+          <S.Label>비밀번호</S.Label>
+          <S.Input type="password" onChange={handlePassword} />
+        </S.LabelInputContainer>
+        <S.LabelInputContainer className="confirm">
+          <S.Label>비밀번호 확인</S.Label>
+          <S.Input type="password" onChange={handleConfirmedPassword} />
+        </S.LabelInputContainer>
+        {password !== confirmed && confirmed !== "" && (
+          <S.PasswordError>비밀번호가 일치하지 않습니다.</S.PasswordError>
+        )}
+        <S.ButtonContainer>
+          <CommonButton
+            contents="변경하기"
+            onClickFn={() => console.log("ddd")}
+            width="20%"
+            height="45px"
+          />
+        </S.ButtonContainer>
+      </S.ChangeContainer>
     </S.Container>
   );
 }
--- src/styles/Profile.styles.ts
+++ src/styles/Profile.styles.ts
@@ -5,9 +5,18 @@ import { colors } from "./palette";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
+`;
+
+export const ChangeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  height: 60%;
 `;
 
 export const Title = styled.div`
@@ -15,28 +24,28 @@ export const Title = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  font-size: ${font.fontSize[700]};
+  font-size: 2.8rem;
   font-weight: 700;
-  margin-top: 180px;
-  margin-bottom: 70px;
+  color: ${colors.blue[500]};
+  margin-bottom: 50px;
 `;
 
 export const LabelInputContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 50%;
+  width: 100%;
   gap: 30px;
-  margin-bottom: 70px;
+  margin-bottom: 30px;
 
-  &.first {
-    margin-bottom: 30px;
+  &.confirm {
+    margin-bottom: 10px;
   }
 `;
 
 export const Label = styled.label`
   font-size: ${font.fontSize[600]};
-  font-weight: 500;
+  font-weight: 600;
   width: 20%;
   text-align: right;
 `;
@@ -44,5 +53,32 @@ export const Label = styled.label`
 export const Input = styled.input`
   width: 50%;
   height: 30px;
+  padding: 5px 10px;
   border: 1px solid ${colors.blue[300]};
+  font-size: ${font.fontSize[200]};
+
+  &:focus {
+    outline: 1px solid ${colors.blue[300]};
+  }
+
+  &.branch {
+    font-weight: 600;
+  }
+`;
+
+export const PasswordError = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  font-size: ${font.fontSize[200]};
+  color: red;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  margin-top: 50px;
 `;
