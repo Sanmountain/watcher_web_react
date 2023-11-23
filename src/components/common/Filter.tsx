185e6a947c62113a2cc3a80113f350c17cbc3a55
HyerinJeon <hyerin0930@gmail.com>
2023-11-23 17:43:03 +0900

    엑셀파일명 수정

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -108,7 +108,7 @@ export default function Filter({
   };
 
   const handleDownloadExcel = () => {
-    excelDownload(filterOption, workList);
+    excelDownload(login.branchName, filterOption, workList);
   };
 
   return (
