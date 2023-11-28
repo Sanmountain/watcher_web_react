e20b239018134cb5e2528b616f53f0e247358fe6
HyerinJeon <hyerin0930@gmail.com>
2023-11-28 16:23:32 +0900

    getMenuList 수정

--- src/utils/getMenuList.ts
+++ src/utils/getMenuList.ts
@@ -3,14 +3,53 @@ import { ILoginState } from "../types/Login.types";
 export const getMenuList = (login: ILoginState) => {
   let menuList;
 
-  if (login.camUsable === "0") {
+  // NOTE PDA 사용 지점, 이미지 사용하지 않는 지점
+  if (login.camStatus === "0") {
     if (login.isAdmin) {
       menuList = [
         { label: "송장조회", path: "/work" },
         { label: "이미지설정", path: "/imageSetting" },
       ];
     } else menuList = [{ label: "송장조회", path: "/work" }];
-  } else if (login.camUsable === "1" || login.camUsable === "3") {
+  }
+
+  // NOTE 오토스캐너 사용 지점 = sk
+  else if (login.camUsable === "1") {
+    if (login.isAdmin) {
+      if (login.camStatus === "0") {
+        menuList = [
+          { label: "송장조회", path: "/work" },
+          { label: "이미지설정", path: "/imageSetting" },
+        ];
+      } else if (login.camStatus === "1") {
+        menuList = [
+          { label: "송장조회", path: "/work" },
+          { label: "이미지조회", path: "/image" },
+          { label: "이미지설정", path: "/imageSetting" },
+        ];
+      }
+    } else {
+      if (login.camStatus === "0") {
+        menuList = [{ label: "송장조회", path: "/work" }];
+      } else if (login.camStatus === "1") {
+        menuList = [
+          { label: "송장조회", path: "/work" },
+          { label: "이미지조회", path: "/image" },
+        ];
+      }
+    }
+  }
+
+  // NOTE 오토스캐너+CCTV 사용 지점
+  else if (login.camUsable === "2") {
+    menuList = [
+      { label: "송장조회", path: "/work" },
+      { label: "화물추적", path: "/vass" },
+    ];
+  }
+
+  // NOTE 이미지 조회 기능 추가 구매 지점
+  else if (login.camStatus === "1") {
     if (login.isAdmin) {
       menuList = [
         { label: "송장조회", path: "/work" },
@@ -21,18 +60,6 @@ export const getMenuList = (login: ILoginState) => {
       menuList = [
         { label: "송장조회", path: "/work" },
         { label: "이미지조회", path: "/image" },
-      ];
-  } else if (login.camUsable === "2") {
-    if (login.isAdmin) {
-      menuList = [
-        { label: "송장조회", path: "/work" },
-        { label: "화물추적", path: "/vass" },
-        { label: "이미지설정", path: "/imageSetting" },
-      ];
-    } else
-      menuList = [
-        { label: "송장조회", path: "/work" },
-        { label: "화물추적", path: "/vass" },
       ];
   }
 
